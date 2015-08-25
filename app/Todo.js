40248f4207ab08764b7dac9ef5ccb62469d198cb
Luke Lancaster <luketlancaster@gmail.com>
2015-08-25 13:47:39 -0500

    loop over and remove todo items

--- app/Todo.js
+++ app/Todo.js
@@ -3,4 +3,17 @@ var app = angular.module("TodoApp", []);
 app.controller("TodoCtrl", function($scope) {
   $scope.title = "Welcome to New York";
   $scope.macaroni = "";
+
+  $scope.todos = [
+    { "name": "Mow the lawn", "complete": false },
+    { "name": "Cut the grass", "complete": false },
+    { "name": "Trim the weeds", "complete": false }
+  ];
+
+  $scope.killTodo = function(todo) {
+    var todoIndex = $scope.todos.indexOf(todo);
+    if (todoIndex >= 0) {
+      $scope.todos.splice(todoIndex, 1);
+    }
+  };
 });
