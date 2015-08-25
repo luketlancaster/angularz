fddbc986aef76a6a4ff235259f4246ea29dabb79
Luke Lancaster <luketlancaster@gmail.com>
2015-08-25 13:56:08 -0500

    add todos to dom

--- app/Todo.js
+++ app/Todo.js
@@ -3,6 +3,7 @@ var app = angular.module("TodoApp", []);
 app.controller("TodoCtrl", function($scope) {
   $scope.title = "Welcome to New York";
   $scope.macaroni = "";
+  $scope.newTodo = "";
 
   $scope.todos = [
     { "name": "Mow the lawn", "complete": false },
@@ -16,4 +17,9 @@ app.controller("TodoCtrl", function($scope) {
       $scope.todos.splice(todoIndex, 1);
     }
   };
+
+  $scope.addTodo = function() {
+    $scope.todos.push({name: $scope.newTodo, complete: false});
+    $scope.newTodo = "";
+  }
 });
